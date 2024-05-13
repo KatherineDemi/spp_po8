078b849b4481d5b2cd216deeecb729ca778b9a0e
KatherineDemi <demchikkate3@gmail.com>
2024-05-13 12:55:45 +0300

    fix lab1 task2 [Demchick]

--- reports/Demchik/1/src/Task2.java
+++ reports/Demchik/1/src/Task2.java
@@ -1,59 +1,55 @@
-import java.util.Scanner;
 import java.util.Random;
 
 public class Task2 {
-    public void printMatrix(double[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++)
-                System.out.print(matrix[i][j] + " ");
-            System.out.println();
-        }
-    }
-
-    public double[] randomPerturbations(double[] vector) {
-        double[] perturbate = new double[vector.length];
-        System.arraycopy(vector, 0, perturbate, 0, vector.length);
-        Random rnd = new Random();
-        for (int i = perturbate.length - 1; i > 0; i--) {
-            int index = rnd.nextInt(i + 1);
-            double temp = perturbate[index];
-            perturbate[index] = perturbate[i];
-            perturbate[i] = temp;
-        }
-        return perturbate;
-    }
-
-    public double[][] randomPerturbations(double[][] matrix) {
-        double[][] perturbate = new double[matrix.length][];
-        for (int i = 0; i < matrix.length; i++) {
-            perturbate[i] = randomPerturbations(matrix[i]);
-        }
-        return perturbate;
-    }
-
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of rows (N): ");
-        int N = scanner.nextInt();
-
-        System.out.print("Enter the number of columns (M): ");
-        int M = scanner.nextInt();
-
-        Task2 contr = new Task2();
-
-        double[][] matrix = new double[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                matrix[i][j] = Math.round(Math.random() * 50);
+        // Пример использования метода randomPerturbations
+        // Создаем матрицу 3x3
+        double[][] matrix = {
+            {1.0, 2.0, 3.0},
+            {4.0, 5.0, 6.0},
+            {7.0, 8.0, 9.0}
+        };
+        
+        // Выводим исходную матрицу
+        System.out.println("Исходная матрица:");
+        printMatrix(matrix);
+        
+        // Вызываем метод randomPerturbations для случайного изменения матрицы
+        randomPerturbations(matrix);
+        
+        // Выводим измененную матрицу
+        System.out.println("\nМатрица после случайных изменений:");
+        printMatrix(matrix);
+    }
+    
+    // Метод для случайного переставления элементов матрицы
+    public static void randomPerturbations(double[][] matrix) {
+        Random random = new Random();
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        // Перебираем элементы матрицы и случайно меняем их местами
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                int randRow = random.nextInt(rows);
+                int randCol = random.nextInt(cols);
+                
+                // Меняем местами элементы matrix[i][j] и matrix[randRow][randCol]
+                double temp = matrix[i][j];
+                matrix[i][j] = matrix[randRow][randCol];
+                matrix[randRow][randCol] = temp;
             }
         }
-
-        System.out.println("Matrix: ");
-        contr.printMatrix(matrix);
-        System.out.println("Perturbated Matrix: ");
-        contr.printMatrix(contr.randomPerturbations(matrix));
-
-        scanner.close(); // Закрываем Scanner после использования
+    }
+    
+    // Метод для вывода матрицы в консоль
+    public static void printMatrix(double[][] matrix) {
+        for (double[] row : matrix) {
+            for (double elem : row) {
+                System.out.print(elem + " ");
+            }
+            System.out.println();
+        }
     }
 }
